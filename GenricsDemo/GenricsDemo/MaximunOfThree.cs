4c4083a67a014adcf87910e619c7bcd4d6f27dc0
unknown <Saitharala@gmail.com>
2021-11-19 22:42:53 +0530

    [ Tharala SaiPrashanth] Added Maximum Float Feauture

--- GenricsDemo/GenricsDemo/MaximunOfThree.cs
+++ GenricsDemo/GenricsDemo/MaximunOfThree.cs
@@ -4,9 +4,24 @@ using System.Text;
 
 namespace GenricsDemo
 {
-    class MaximunOfThree
+    public class MaximumComputation
     {
         public static int FindMax(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            else
+            {
+                return thirdNumber;
+            }
+        }
+        public static double FindMax(double firstNumber, double secondNumber, double thirdNumber)
         {
             if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
             {
